06abdda624be0e111a1ead89d67c5b9d737637e9
hassan-kurwa-buruhani <hassankurwaburuhani@gmail.com>
2025-05-08 10:59:33 +0300

    day one ....basics of globals and modules2

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // console.log("Hello World");
 
-const sayHi = require('./1-sayHi');
-const names = require('./2-names');
-require('./3-addnos');
+// const sayHi = require('./1-sayHi');
+// const names = require('./2-names');
+// require('./3-addnos');
 
-sayHi(names.john);
+// sayHi(names.john);
+
+
+const math = require('./4-mathmodule');
+console.log(math.add(10, 20));
+console.log(math.subtract(10, 20));
+console.log(math.multiply(10, 20));
+console.log(math.divide(10, 20));
 
